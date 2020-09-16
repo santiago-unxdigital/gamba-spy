b37c36ba7be2cc5d68187189d0a64c64742daf87
Santiago Tamagno <>
2020-09-16 14:14:59 -0300

    fixing routes

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('spy_artist/', include('spy_artist.urls')),
+    path('', include('spy_artist.urls')),
     path('admin/', admin.site.urls),
 ]
