8d1f6e281d9f677544c6e2eca6ad7160a63b394d
Santiago Tamagno <>
2020-09-16 11:36:20 -0300

    first commit

---
+++ site/spy_artist/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpyArtistConfig(AppConfig):
+    name = 'spy_artist'
---
+++ site/spy_artist/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseRedirect
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import time 
+import sys
+from pprint import pprint
+from . import forms
+
+'''
+def index(request, artist):
+        client_id = '851985a4c736425fac5e6c803c3c9055'
+        client_secret = '86c6552c29664f5aa3b284ecece085b0'
+        client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+        sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+        search_str = artist
+        result = sp.search(search_str,1,0,'artist')
+        artist = result['artists']['items'][0]
+        
+        context = {'result': artist}
+        return render(request, 'spy_artist/index.html', context)
+'''
+
+def index(request):
+    form = forms.InputForm()
+    return render(request, 'index.html', {'form': form})
+
+def addition(a,b):
+    c = a + b
+    return c
+
+def output(request):
+    if request.method == 'POST':
+        form = forms.InputForm(request.POST)
+        if form.is_valid():
+            input1 = form.cleaned_data['input1']
+
+            client_id = '851985a4c736425fac5e6c803c3c9055'
+            client_secret = '86c6552c29664f5aa3b284ecece085b0'
+            client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+            sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+            search_str = input1
+            result = sp.search(search_str,1,0,'artist')
+
+
+            try:
+                artist = result['artists']['items'][0]
+            except:
+                artist = None
+
+            context = {'artist': artist}
+
+            return render(request, 'output.html', context)
+    else:
+            form = forms.InputForm()
